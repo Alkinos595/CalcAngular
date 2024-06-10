2e0f0cdcc69b4afff1f6f97017ba48ce22eb7ff0
Alberto Foinquinos <foinquinosalberto@gmail.com>
2024-06-10 08:32:49 +0200

    add feature added

--- src/app/modules/calculator/addition/addition.component.ts
+++ src/app/modules/calculator/addition/addition.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-addition',
 	standalone: true,
-	imports: [],
+	imports: [FormsModule],
 	templateUrl: './addition.component.html'
 })
 export class AdditionComponent {
-	public num1:number = 0;
-	public num2:number = 0;
 
-	/* function add(number1:number, number2:number) {
-		return number1 + number2;
-	} */
+	/* public num1!:number;
+	public num2!:number;
+
+	result: number = 0;
+	public add():number{
+		console.log(this.num1);
+		console.log(this.num2);
+		this.result = (this.num1) + (this.num2);
+		console.log(result);
+		return(result);
+		}  */
+		public num1!: number;
+		public num2!: number;
+		public result: number = 0;
+	  
+		public add(): void {
+		  console.log(this.num1);
+		  console.log(this.num2);
+		  this.result = this.num1 + this.num2;
+		  console.log(this.result);
+		}
 }
